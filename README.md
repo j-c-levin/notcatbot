b8338a456842721d8a23b7122584626873cffb87
Joshua Levin <joshua.c.levin@gmail.com>
2017-08-13 21:39:05 +0100

    all bot builder, all the time

--- README.md
+++
@@ -1,6 +0,0 @@
-# notcatbot
-Not a cat bot for telegram
-
-For testing webhooks locally: ngrok.exe http 3000
-
-Deployment: gcloud app deploy
---
+++ messages/index.js
@@ -0,0 +1,31 @@
+"use strict";
+var builder = require("botbuilder");
+var botbuilder_azure = require("botbuilder-azure");
+var path = require('path');
+
+var useEmulator = (process.env.NODE_ENV == 'development');
+
+var connector = useEmulator ? new builder.ChatConnector() : new botbuilder_azure.BotServiceConnector({
+    appId: process.env['MicrosoftAppId'],
+    appPassword: process.env['MicrosoftAppPassword'],
+    stateEndpoint: process.env['BotStateEndpoint'],
+    openIdMetadata: process.env['BotOpenIdMetadata']
+});
+
+var bot = new builder.UniversalBot(connector);
+bot.localePath(path.join(__dirname, './locale'));
+
+bot.dialog('/', function (session) {
+    session.send('You said ' + session.message.text);
+});
+
+if (useEmulator) {
+    var restify = require('restify');
+    var server = restify.createServer();
+    server.listen(3978, function() {
+        console.log('test bot endpont at http://localhost:3978/api/messages');
+    });
+    server.post('/api/messages', connector.listen());    
+} else {
+    module.exports = { default: connector.listen() }
+}
