ea766425b99e72a5be0c18791d1e4c490d227684
Joshua Levin <joshua.c.levin@gmail.com>
2017-08-25 11:08:41 +0100

    parse imgur feeds so that everything looks nicer

--- messages/dogs.js
+++ messages/dogs.js
@@ -1,7 +1,7 @@
 'use strict';
 const fs = require('fs');
 const path = require('path');
-const dogData = JSON.parse(fs.readFileSync(path.resolve(__dirname, 'dogs.json'), 'utf-8'));
+const imageData = JSON.parse(fs.readFileSync(path.resolve(__dirname, 'parsed_images.json'), 'utf-8'));
 
 // Gets a random integer within a range, taken from http://stackoverflow.com/a/1527820
 function getRandomInt(min, max) {
@@ -10,14 +10,8 @@ function getRandomInt(min, max) {
 
 module.exports = {
     getDogPhoto : () => {
-        let responseUrl;
-        const albumNumber = getRandomInt(0, dogData.data.length - 1);
-        if (dogData.data[albumNumber].is_album) {
-            const imageNumber = getRandomInt(0, dogData.data[albumNumber].images.length - 1);
-            responseUrl = dogData.data[albumNumber].images[imageNumber].link;
-        } else if (dogData.data[albumNumber].is_album === false) {
-            responseUrl = dogData.data[albumNumber].link;
-        }
-        return responseUrl;
+        const imageNumber = getRandomInt(0, imageData.length - 1);
+        console.log('number: ' + imageNumber + ' link: ' + imageData[imageNumber].link);
+        return imageData[imageNumber].link;
     }
 }
