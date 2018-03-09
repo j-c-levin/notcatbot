4e55fa3ee25da36418d8677cfc38c2eea13a0086
Joshua Levin <joshua.c.levin@gmail.com>
2018-03-09 23:46:48 +0000

    add telegraf

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,30 @@
-// hello world
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const telegraf = require("telegraf");
+const ngrok = require("ngrok");
+const dotenv = require("dotenv");
+const index_1 = require("./cat/index");
+dotenv.config();
+function init() {
+    return __awaiter(this, void 0, void 0, function* () {
+        const bot = new telegraf(process.env.BOT_TOKEN);
+        let url = (typeof process.env.DEVELOPMENT !== 'undefined') ? yield ngrok.connect(80) : process.env.URL;
+        setupHandlers(bot);
+        bot.telegram.setWebhook(url + "/secret");
+        bot.startWebhook("/secret", null, 80);
+        console.log("bot running");
+    });
+}
+function setupHandlers(bot) {
+    index_1.CatResponse.setupHandlers(bot);
+}
+init();
 //# sourceMappingURL=index.js.map
--- source/index.ts
+++ source/index.ts
@@ -1,1 +1,20 @@
-// hello world
+import * as telegraf from "telegraf";
+import * as ngrok from "ngrok";
+import * as dotenv from "dotenv";
+import { CatResponse } from "./cat/index";
+dotenv.config();
+
+async function init() {
+  const bot = new telegraf(process.env.BOT_TOKEN);
+  let url = (typeof process.env.DEVELOPMENT !== 'undefined') ? await ngrok.connect(80) : process.env.URL ;
+  setupHandlers(bot);
+  bot.telegram.setWebhook(url + "/secret");
+  bot.startWebhook("/secret", null, 80);
+  console.log("bot running");
+}
+
+function setupHandlers(bot: any): void {
+  CatResponse.setupHandlers(bot);
+}
+
+init();
