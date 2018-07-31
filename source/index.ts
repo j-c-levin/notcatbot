807b7f283417b233127a20f8ff425ba62559de07
Joshua Levin <joshua.c.levin@gmail.com>
2018-07-31 20:32:15 +0100

    start the migration

--- source/index.ts
+++ source/index.ts
@@ -1,44 +1,7 @@
-import * as telegraf from "telegraf";
-import * as ngrok from "ngrok";
 import * as dotenv from "dotenv";
-import { DogResponse } from "./handlers/dog";
-import { HelpResponse } from './handlers/help';
-import { HugResponse } from './handlers/hug';
-import { HoroscopeResponse } from "./handlers/horoscope";
-import { ChatEventResponse } from "./handlers/chat_event";
-import { ShrugResponse } from './handlers/shrug';
-import { FlipResponse } from './handlers/flip';
-import { PatResponse } from './handlers/pat';
-import { PigResponse } from './handlers/pig';
-import { FunSlapResponse } from './handlers/funslap';
+import * as botBuilder from 'claudia-bot-builder';
 dotenv.config();
 
-async function init() {
-  // Initialise the bot
-  const bot = new telegraf(process.env.BOT_TOKEN);
-  // disable username because it wasn't allowing commands through properly
-  // bot.telegram.getMe().then(() => { bot.options.username = process.env.BOT_NAME; });
-  // Start ngrok if not deployed
-  const url = (typeof process.env.DEVELOPMENT !== 'undefined') ? await ngrok.connect(80) : process.env.URL;
-  // Set up the commands the bot will respond too
-  setupHandlers(bot);
-  // Set up and start the webhook
-  bot.telegram.setWebhook(`${url}/${process.env.SECRET_PATH}`);
-  bot.startWebhook(`/${process.env.SECRET_PATH}`, null, process.env.PORT);
-  console.log("bot running");
-}
-
-function setupHandlers(bot: any): void {
-  DogResponse.setupHandlers(bot);
-  HelpResponse.setupHandlers(bot);
-  HoroscopeResponse.setupHandlers(bot);
-  HugResponse.setupHandlers(bot);
-  ChatEventResponse.setupHandlers(bot);
-  ShrugResponse.setupHandlers(bot);
-  FlipResponse.setupHandlers(bot);
-  PatResponse.setupHandlers(bot);
-  PigResponse.setupHandlers(bot);
-  FunSlapResponse.setupHandlers(bot);
-}
-
-init();
+module.exports = botBuilder((request) => {
+  return 'hello world, ' + request.text;
+});
