30386d2448d9dbb9f53e77b781cbab264b65f982
Joshua Levin <joshua.c.levin@gmail.com>
2019-10-18 11:45:48 +0100

    Update index.ts

--- source/handlers/chat_event/index.ts
+++ source/handlers/chat_event/index.ts
@@ -7,6 +7,7 @@ export function leftChatEvent() {
 
 export function joinedChatEvent(): string[] {
   return [
+    new telegramTemplate.Pause(5000).get(),
     `Hey, welcome to the dollars' telegram group!  I'm just a bot, but I'm sure others will welcome you in their own bizzare manner soon :)`,
     new telegramTemplate.Pause(500).get(),
     `We don't have many rules, mainly we just like to keep the chat drama free and SFW (Safe For Work).  Other than that, welcome and enjoy!`
